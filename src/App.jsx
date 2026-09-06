d0c2944dbafbc0082cb4c6520b8640b4122da245
Shakib <mdshakib771575@gmail.com>
2026-09-06 12:19:22 +0600

    implemented conditional randar and array props

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import './App.css'
 import Card from './components/Card'
 // import TestComponet from './TestComponet'
 
+const members = [
+  { Name: "Shakib", age: 21 },
+  { Name: "bakib", age: 24 },
+  { Name: "nokib", age: 27 },
+  { Name: "Akib", age: 29 },
+]
+
 function App() {
- 
+
 
   return (
     <>
-    {/* <TestComponet></TestComponet> */}
-    <Card fristName="shakib" lastName="Hazari"><h1>Hello World</h1></Card>
-    
+      {/* <TestComponet></TestComponet> */}
+      {members.sort((a, b) => a.age - b.age)
+        .filter((mem => mem.age > 23))
+        .map((member) => <Card name={member.Name} age={member.age}></Card>)}
+
+        {/* <Card members={members}></Card> */}
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
-
+import "./Style.css"
 // const Card = (props) => {
 //     console.log(props);
 //     return (
 //         <div>
 //             <p>FristName : {props.fristName}</p>
 //             <p>FristName : {props.lastName}</p>
-            
+
 //         </div>
 //     );
 // };
@@ -13,17 +13,63 @@
 // export default Card;
 
 
+ export default function Card ({Name,age})  {
 
-const Card = ({fristName,lastName,children}) => {
-    
-    return (
-        <div>
-            {children}
-            <p>FristName : {fristName}</p>
-            <p>FristName : {lastName}</p>
-            
-        </div>
-    );
+    // if (age > 18) {
+    //     return (
+    //         <div>
+
+    //             <p>FristName : {Name}</p>
+    //             <p>FristName : {age}</p>
+
+    //         </div>
+    //     );
+    // }
+    // if (age < 18) {
+    //     return "you are young"
+    // }
+
+
+    // return (
+    //     <>
+    //         {age > 18 && <div>
+    //             <p>FristName : {Name}</p>
+    //             <p>FristName : {age}</p>
+    //         </div>
+    //         }
+    //         {age < 18 && "you are young"}
+    //     </>
+    // )
+
+      return (
+        <>
+            {age > 18 ? <div className="style">
+                <p>FristName : {Name}</p>
+                <p>FristName : {age}</p>
+            </div>
+            : <div className="style">you are young</div>}
+        </>
+    )
 };
 
-export default Card;
+
+
+
+// export default function Card ({members})  {
+//     console.log(members);
+
+//       return (
+//         <>
+//          {members.sort((a, b) => a.age - b.age)
+//         .filter((mem => mem.age > 23))
+//         .map((member) => member.age > 18 ? <div className="style">
+//                 <p>FristName : {member.Name}</p>
+//                 <p>FristName : {member.age}</p>
+//             </div>
+//             : <div className="style">you are young</div>)} 
+//         </>
+//     )
+// };
+
+
+
